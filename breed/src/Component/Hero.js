e6c96de880f1d48325e682ba271b0fc68bac570f
Pere-Isenah <pereisenah@gmail.com>
2023-11-19 13:19:44 +0100

    updated src files

---
+++ breed/src/Component/Hero.js
@@ -0,0 +1,43 @@
+import DogVideo from '../mixkit-smiling-dog-1552-medium.mp4';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import React from "react";
+
+
+export const Hero = () => {
+  return (
+    <>
+	<video autoplay="autoplay" loop="loop"><source src={DogVideo} type="video/mp4"/></video>
+	<div class="carousel slide" data-bs-ride="carousel" id="carouselExampleCaptions">
+		<div class="carousel-indicators">
+			<button aria-label="Slide 1" class="active" data-bs-slide-to="0" data-bs-target="#carouselExampleCaptions" type="button"></button> <button aria-label="Slide 2" data-bs-slide-to="1" data-bs-target="#carouselExampleCaptions" type="button"></button> <button aria-label="Slide 3" data-bs-slide-to="2" data-bs-target="#carouselExampleCaptions" type="button"></button>
+		</div>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<div class="carousel-caption">
+					<h5 class="animated fadeInDown" style="animation-delay: 1s">Experienced Doctors</h5>
+					<p class="animated fadeInUp d-none d-md-block" style="animation-delay: 2s">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime tempore quis esse quidem fugiat cum.</p>
+					<p class="animated fadeInUp" style="animation-delay: 3s"><a href="#">Appointment</a></p>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<div class="carousel-caption">
+					<h5 class="animated fadeInDown" style="animation-delay: 1s">Modern Machineries</h5>
+					<p class="animated fadeInUp d-none d-md-block" style="animation-delay: 2s">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime tempore quis esse quidem fugiat cum.</p>
+					<p class="animated fadeInUp" style="animation-delay: 3s"><a href="#">Appointment</a></p>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<div class="carousel-caption">
+					<h5 class="animated fadeInDown" style="animation-delay: 1s">24 / 7 Services</h5>
+					<p class="animated fadeInUp d-none d-md-block" style="animation-delay: 2s">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime tempore quis esse quidem fugiat cum.</p>
+					<p class="animated fadeInUp" style="animation-delay: 3s"><a href="#">Appointment</a></p>
+				</div>
+			</div>
+		</div><button class="carousel-control-prev" data-bs-slide="prev" data-bs-target="#carouselExampleCaptions" type="button"><span aria-hidden="true" class="carousel-control-prev-icon"></span> <span class="visually-hidden">Previous</span></button> <button class="carousel-control-next" data-bs-slide="next" data-bs-target="#carouselExampleCaptions" type="button"><span aria-hidden="true" class="carousel-control-next-icon"></span> <span class="visually-hidden">Next</span></button>
+	</div>
+</>
+  );
+};
--- breed/src/index.js
+++ breed/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {Hero} from './Component/Hero';
+import {Nav} from './Component/Nav';
+import "./Dog.css";
 import reportWebVitals from './reportWebVitals';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  
+    <Nav />
+    <Hero />
   </React.StrictMode>
 );
 
